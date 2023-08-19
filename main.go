88c823b098fda857eb1fd7bfcfd1373f93cd4954
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-19 17:32:02 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func main() {
+	server := gin.Default()
+
+
+	server.GET("webhook", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H {"success": "webhook spun here"})
+	})
+
+
+	server.Run(":8080")
+}
