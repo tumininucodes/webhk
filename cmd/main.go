5ca4a9ae4429ee1c6b566a0620858f35210971cc
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2023-08-23 16:52:49 +0100

    development

--- cmd/main.go
+++ cmd/main.go
@@ -12,18 +12,13 @@ func main() {
 	server := gin.Default()
 
 	server.POST("webhook", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"success": "webhook spun here"})
-
 		var webhookData map[string]interface{}
-
 		ctx.ShouldBindJSON(&webhookData)
-
 		fmt.Println("webhook data -> ", webhookData)
-
 	})
 
 	server.GET("", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"success": "heher spun here"})
+		ctx.JSON(http.StatusOK, gin.H{"success": "the world is yours for the taking"})
 	})
 
 	server.Run(":8080")
